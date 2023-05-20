33aa2f5650ad85f396d2a64e707e7d9fe4a0ed20
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-05-20 18:07:43 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -1,22 +1,61 @@
 var mongoose = require("mongoose");
 
 /*
+
 Complete your other field here
 
 1. "heading" should be a string and required
+
 2. "description" should be a string and required
+
 3. "status" should be a string and it can have only 2 values ('pending', 'done') and default should be pending
+
 4. "creator_id" will be reference to user who have created this task (store the user id of creator).
+
 5. Also Enable timestamps which will store the date of document created and updated.
 
 */
 
-
 var taskSchema = mongoose.Schema({
+
   //Write your code here.
+
+  heading:{
+
+    type: String,
+
+    required: true
+
+  },
+
+  description:{
+
+    type: String,
+
+    required: true
+
+  },
+
+  status:{
+
+    type: String,
+
+    enum:["pending", "done"],
+
+    default:"pending"
+
+  },
+
+  creator_id:{
+
+    type: String,
+
+  }
+
 },
-{ /* ... */ }
+
+{ timestamps: true }
+
 );
 
-
-module.exports = mongoose.model("Task", taskSchema);;
+module.exports = mongoose.model("Task", taskSchema);
